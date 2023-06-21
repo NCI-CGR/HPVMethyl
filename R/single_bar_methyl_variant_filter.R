0ff5075fbe049aa3edf4ccd88d04294d57480ada
weizhu365 <zhuw10@nih.gov>
2023-06-21 14:18:02 +0800

    fix(R/single_bar_methyl_variant_filter.R): fix methyl_freq wo genotype call (set as NA); rename; save two copies

--- R/single_bar_methyl_variant_filter.R
+++ R/single_bar_methyl_variant_filter.R
@@ -2,6 +2,8 @@ single_bar_methyl_variant_filter <- function(variants, filteringTablePath, posCo
   
   require(fuzzyjoin)
   
+  manifest <- manifest %>% filter(!is.na(Owner_Sample_ID))
+
   manifest %>%
   transform(BC1 = as.character(BC1)) %>% 
     rename(barcode = BC1) -> manifest
@@ -66,12 +68,12 @@ single_bar_methyl_variant_filter <- function(variants, filteringTablePath, posCo
     mutate(methyl_freq = ifelse(status == "Fail","NA",methyl_freq)) %>%
     select(chr_amplicon, pos_amplicon, pos_from_conversion_table = pos, DP, methyl_freq, QUAL, status, qc_reason, everything()) 
   
-  return_table = manifest %>% 
+  pre_return_table = manifest %>% 
     left_join(filtered_variants %>% select(chr_amplicon,pos_amplicon,chr,pos_from_conversion_table,REF,ALT,DP,methyl_freq,status,qc_reason,QUAL,FILTER,CpG_Variant_Info,HS,TYPE,HRUN,everything())) %>% 
     filter(!(is.na(Owner_Sample_ID))) 
   
-  
-  coverage_matrix = return_table %>% 
+  ### focus on the depth (nothing to do with mehtyl_freq)
+  coverage_matrix = pre_return_table %>% 
     group_by(Owner_Sample_ID, barcode, chr_amplicon) %>% 
     summarize(depth = max(DP)) %>% 
     ungroup() %>% 
@@ -85,6 +87,8 @@ single_bar_methyl_variant_filter <- function(variants, filteringTablePath, posCo
     spread(chr_amplicon, depth) %>% 
     glimpse() 
   
+
+  ### Get the column order from control_defs and use it to reorder coverage_majtrix
   control_defs %>%
     select(-control_code,-control_type) %>%
     colnames() -> con_sort_order
@@ -92,6 +96,7 @@ single_bar_methyl_variant_filter <- function(variants, filteringTablePath, posCo
   coverage_matrix = coverage_matrix %>%
     select(Owner_Sample_ID, barcode, total_HPV_reads,total_human_reads, total_MASIC_reads, con_sort_order, everything())
   
+  ### simple join manifest table to have the output for coverage_matrix_results.csv
   manifest %>%
     inner_join(coverage_matrix) %>%
     filter(!(is.na(Owner_Sample_ID))) %>%
@@ -99,11 +104,13 @@ single_bar_methyl_variant_filter <- function(variants, filteringTablePath, posCo
   
   
 # Prepare the pn_filter
+# chrom Min_reads_per_type
   pn_filter<-read.csv(pn_filters)
   pn_filter %>%
     rename(chrom = contig) -> pn_filters
  
-  
+# coverage_matrix + pn_filters => detailed_pn_matirx
+# pos status is assigned for each amplicon.
  coverage_matrix %>% 
     gather(chrom,depth,-Owner_Sample_ID,-barcode,-total_HPV_reads,-total_MASIC_reads,-total_human_reads) %>%
     inner_join(pn_filters) %>%
@@ -111,7 +118,8 @@ single_bar_methyl_variant_filter <- function(variants, filteringTablePath, posCo
     select(-Min_reads_per_type,-depth) %>% 
     spread(chrom,status) -> detailed_pn_matrix
  
-#Calculate num_type_pos separately   
+#Calculate num_type_pos separately (the code was not well-written) 
+# to add Num_Types_Pos 
     
     num_type_list <- detailed_pn_matrix %>%
     tidyr::gather("type_id", "type_status", starts_with("HPV")) %>%  #Only counting HPV contigs and not MASIC contigs here
@@ -134,16 +142,13 @@ single_bar_methyl_variant_filter <- function(variants, filteringTablePath, posCo
       inner_join(detailed_pn_matrix) %>% 
       transform(barcode = as.character(barcode)) 
     
+    ### to add Num_Types_Pos to detailed_pn_matrix_results
     manifest %>%
       inner_join(num_type_list) %>% 
       filter(!(is.na(Owner_Sample_ID))) %>%
       write.csv("detailed_pn_matrix_results.csv", row.names = F)
+       
     
-    
-    return_table = return_table %>%
-      inner_join(num_type_list %>% select(Owner_Sample_ID, barcode, Num_Types_Pos) %>% transform(Num_Types_Pos = as.integer(Num_Types_Pos)),by = c("Owner_Sample_ID","barcode")) %>%
-      mutate(methyl_freq = ifelse(Num_Types_Pos == 1, "NA",methyl_freq))
-    write_csv(return_table,"target_variants_results.csv")
     
 
   # Simple pn matrix
@@ -168,6 +173,19 @@ single_bar_methyl_variant_filter <- function(variants, filteringTablePath, posCo
   simple_pn_matrix = simple_pn_matrix_long %>%
     spread(type, simple_status)
     
+  ### return table is changed to pre_return_table and tvr_table (for the final return)
+  ### update qc_reason and status too
+    
+    tvr_table = pre_return_table %>%
+      inner_join(num_type_list %>% select(Owner_Sample_ID, barcode, Num_Types_Pos) %>% transform(Num_Types_Pos = as.integer(Num_Types_Pos)),by = c("Owner_Sample_ID","barcode")) %>% 
+      left_join( simple_pn_matrix_long %>% ungroup %>% select(barcode, Gene=type, simple_status) )  %>%
+      mutate(qc_reason = ifelse(!is.na(simple_status) & simple_status == 'neg' , ifelse(methyl_freq %in% c("0", "NA"), ifelse(qc_reason=="Pass", "Fail", qc_reason), "Fail;false-positive"),qc_reason)) %>%
+      mutate(methyl_freq = ifelse(!is.na(simple_status) & simple_status == 'neg', "NA",methyl_freq)) %>%
+      mutate(status = ifelse(!is.na(simple_status) & simple_status == 'neg', "Fail",status))
+
+    write_csv(tvr_table,"target_variants_internal_results.csv")
+
+  # to have a count table for masic1/2: Owner_Sample_ID     barcode num_MASIC_pos num_MASIC2_pos
   simple_pn_matrix_long %>%
       spread(type, simple_status) %>% 
       gather("MAS_type","value", starts_with("MASIC")) %>%
@@ -181,7 +199,8 @@ single_bar_methyl_variant_filter <- function(variants, filteringTablePath, posCo
       ungroup() %>%
       select(Owner_Sample_ID,barcode,num_MASIC_pos,num_MASIC2_pos) %>%
       distinct() -> count_table
-    
+
+  # to have HPV_count_table: Owner_Sample_ID,barcode,num_HPV_pos  
   simple_pn_matrix_long %>%
       mutate(HPV_pos = ifelse(simple_status == "pos",1,0)) %>%
       group_by(Owner_Sample_ID,barcode) %>%
@@ -189,7 +208,7 @@ single_bar_methyl_variant_filter <- function(variants, filteringTablePath, posCo
       select(Owner_Sample_ID,barcode,num_HPV_pos) %>%
       distinct() -> HPV_count_table
       
-  
+  ### to write simple_pn_matrix_results.csv 
   manifest %>%
     inner_join(count_table) %>%
     inner_join(HPV_count_table) %>% 
@@ -199,8 +218,9 @@ single_bar_methyl_variant_filter <- function(variants, filteringTablePath, posCo
     
   
   #freq_matrix
-  
-  freq_matrix = return_table %>% 
+  # calculate mean methyl_freq of qualified sites in each amplicon
+  # saved in a wide from
+  freq_matrix = tvr_table %>% 
     mutate(methyl_freq = ifelse(Num_Types_Pos == 1, "NA",methyl_freq)) %>% 
     transform(methyl_freq = as.numeric(methyl_freq)) %>%
     group_by(Owner_Sample_ID, barcode, chr_amplicon) %>%
@@ -216,12 +236,26 @@ single_bar_methyl_variant_filter <- function(variants, filteringTablePath, posCo
     filter(!(is.na(Owner_Sample_ID))) %>%
     write_csv("freq_matrix_results.csv") 
   
-  
+  ### Convert control_defs from wide form to the long form
+  # with the 4 columns: 
+# $ control_code <chr> "HEK", "human", "hg19_all-HR", "NTC", "HEK", "human", ...
+# $ control_type <chr> "pos", "pos", "pos", "neg", "pos", "pos", "pos", "neg"...
+# $ chrom        <chr> "MASIC1_A", "MASIC1_A", "MASIC1_A", "MASIC1_A", "MASIC...
+# $ min_coverage <dbl> 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20...
   control_defs = control_defs %>%
     glimpse() %>%
     tidyr::gather("chrom", "min_coverage", -control_code,-control_type) %>%
     glimpse()
   
+#   control_results[,1:6]
+# # A tibble: 4 x 6
+# # Groups:   Owner_Sample_ID, barcode [4]
+#   Owner_Sample_ID     barcode num_pass_targets num_fail_targets ASCL1 EPB41L3
+#   <chr>               <chr>              <dbl>            <dbl> <chr> <chr>  
+# 1 0perc-hg19_all-HR   259                   55                2 Pass  Pass   
+# 2 100perc-hg19_all-HR 229                   57                0 Pass  Pass   
+# 3 50perc-hg19_all-HR  235                   54                3 Pass  Pass   
+# 4 HEK                 287                   57                0 Pass  Pass   
   control_results = coverage_matrix %>% 
     tidyr::gather("chrom", "depth", -Owner_Sample_ID, -barcode, -starts_with("MASIC"), -total_MASIC_reads, -total_HPV_reads, -total_human_reads) %>% 
     mutate(depth = as.integer(depth)) %>%
@@ -265,7 +299,10 @@ single_bar_methyl_variant_filter <- function(variants, filteringTablePath, posCo
     filter(!(is.na(Owner_Sample_ID))) %>% 
     write_csv("control_results.csv")
   
-  
+### control samples can be identified using Owner_Sample_ID in control_results_final 
+write_csv(tvr_table %>% filter(! Owner_Sample_ID %in% control_results_final$Owner_Sample_ID),"target_variants_external_results.csv")
+
+
   #Control frequency QC
   
   
@@ -305,7 +342,11 @@ single_bar_methyl_variant_filter <- function(variants, filteringTablePath, posCo
     filter(!(is.na(Owner_Sample_ID))) %>%
     write_csv("non_target_variants_results.csv")
   
-  return(return_table)
+  ### rename
+  prefix <- manifest$Assay_Batch_Code[1]
+  cmd <- sprintf("rename 's/^/%s_/' *_results.csv", prefix)
+  system(cmd)
+  return(tvr_table)
   
   
 }
